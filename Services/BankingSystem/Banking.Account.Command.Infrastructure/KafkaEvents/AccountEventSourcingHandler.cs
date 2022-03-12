4336329522cb3345e32a2b3abbaefa21270abe83
Wilmer BZ <wilmerbz@outlook.com>
2022-03-12 11:27:50 -0500

    Added Account Event Sourcing.

---
+++ Services/BankingSystem/Banking.Account.Command.Infrastructure/KafkaEvents/AccountEventSourcingHandler.cs
@@ -0,0 +1,39 @@
+﻿using Banking.Account.Command.Application.Aggregates;
+using Banking.CQRS.Core.Domain;
+using Banking.CQRS.Core.Handlers;
+
+namespace Banking.Account.Command.Infrastructure.KafkaEvents
+{
+    public class AccountEventSourcingHandler : IEventSourcingHandler<AccountAggregate>
+    {
+
+        private readonly AccountEventStore _eventStore;
+
+        public AccountEventSourcingHandler(AccountEventStore eventStore)
+        {
+            _eventStore = eventStore;
+        }
+
+        public async Task<AccountAggregate> GetById(string id)
+        {
+            var aggregate = new AccountAggregate();
+
+            var events = await _eventStore.GetEvents(id);
+
+            if (events != null && events.Any())
+            {
+                aggregate.ReplayEvents(events);
+                var latestVersion = events.Max(e => e.Version);
+                aggregate.Version = latestVersion;
+            }
+
+            return aggregate;
+        }
+
+        public async Task Save(AggregateRoot aggregate)
+        {
+            await _eventStore.SaveEvent(aggregate.Id, aggregate.GetUncommittedChanges(), aggregate.Version);
+            aggregate.MarkChangesAsCommitted();
+        }
+    }
+}
